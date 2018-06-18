5cf522fece0bcd213e19e13aa51f169681ad6b00
kevin <kevinjobrien420@gmail.com>
2018-06-18 18:46:33 -0500

    206 App Number 21 - Part2 - Get the Information of Sports by using Arrays in Java.//Coding

--- app/src/main/java/com/example/kevin/gettheinformationofsportsbyusingarraysinjava/MainActivity.java
+++ app/src/main/java/com/example/kevin/gettheinformationofsportsbyusingarraysinjava/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kevin.gettheinformationofsportsbyusingarraysinjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Create the objects
+
+        TextView txtSportName = (TextView) findViewById(R.id.txtSportName);
+        TextView txtScores = (TextView) findViewById(R.id.txtScores);
+        TextView txtScoresAverage = (TextView) findViewById(R.id.txtScoresAverage);
+        TextView txtHighestScore = (TextView) findViewById(R.id.txtHighestScore);
+        TextView txtLowestScore = (TextView) findViewById(R.id.txtLowestScore);
+
+        int[] scoreArray = {87, 34, 56, 23, 65, 98, 35, 21, 48, 56, 45, 86, 37};
+
+        Sport mySport = new Sport("Boxing", scoreArray);
+        txtSportName.setText(mySport.getSportName());
+        mySport.letsOutPutTheScores(txtScores);
+        txtScoresAverage.setText(mySport.getTheAverage() + "");
+        txtHighestScore.setText(mySport.getTheMaximumValue() + "");
+        txtLowestScore.setText(mySport.getTheMinimumValue() + "");
 
 
 
--- app/src/main/java/com/example/kevin/gettheinformationofsportsbyusingarraysinjava/Sport.java
+++ app/src/main/java/com/example/kevin/gettheinformationofsportsbyusingarraysinjava/Sport.java
@@ -1,5 +1,7 @@
 package com.example.kevin.gettheinformationofsportsbyusingarraysinjava;
 
+import android.widget.TextView;
+
 public class Sport {
 
     //Instance Variables
@@ -34,6 +36,42 @@ public class Sport {
         }
 
         return lowestScore;
+    }
+
+    public int getTheMaximumValue() {
+
+        int highestScore = scores[0]; // Now we are assuming that the scores[index] is the highest score
+        for (int score : scores) {
+            if (score > highestScore) {
+                highestScore = score;
+            }
+        }
+        return highestScore;
+    }
+    // Get the average value
+    public  double getTheAverage() {
+
+        int total = 0;
+
+        for (int score : scores) {
+            total += score;
+            // total = total + score;
+        }
+
+        return (double) total / scores.length;
+    }
+
+    public void letsOutPutTheScores(TextView textView) {
+
+        String oldTextViewValue;
+
+        for (int index = 0; index < scores.length; index++) {
+
+            oldTextViewValue = textView.getText().toString();
+
+            textView.setText(String.format(oldTextViewValue + "Athlete Number: %2d: %3d\n", index, scores[index]));
+
+        }
     }
 
 
